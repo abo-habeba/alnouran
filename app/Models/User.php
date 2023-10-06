4ef53c65fa37c36a6448770197c5a93106d3fdbe
abo Habeba <atsh.phone@gmail.com>
2023-10-06 16:46:49 +0200

    vvvv

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Task;
 use App\Models\Report;
 use App\Models\Station;
 use Laravel\Sanctum\HasApiTokens;
@@ -14,11 +15,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
