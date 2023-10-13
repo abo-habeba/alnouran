aeb5329168e21491836e87b178259161c0db2d03
abo Habeba <atsh.phone@gmail.com>
2023-10-13 14:55:11 +0200

    xxxxxxxxxxxxxxxx

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         // $request->user()->currentAccessToken()->delete();
-        $user = $request->user()->currentAccessToken();
+        $user = $request->user()->currentAccessToken()->delete();
         return response()->json(['msg' => 'Logged out Successfully.', $user]);
         
     }
