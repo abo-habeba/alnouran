61e678c5687bf98bd59586306aac08737ebdcfd7
abo Habeba <atsh.phone@gmail.com>
2023-10-14 00:05:41 +0200

    f

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,7 +78,7 @@ class UserController extends Controller
     {
         $user = $request->user();
         $requestTokenId = intval(explode("|", request()->bearerToken())[0]);
-        $tokenId = $user->tokens()->where('id', $requestTokenId)->get()->id;
+        $tokenId = $user->tokens()->where('id', $requestTokenId)->get()[0]->id;
         // if ($tokenId) {
         return response()->json(["tokenId" => $tokenId, "requestTokenId" => $requestTokenId]);
         // } else {
