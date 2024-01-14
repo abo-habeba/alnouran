09caa80f43a937051c996b691dca1f85aef03698
abo habeba <atsh.phone@gmail.com>
2024-01-14 23:55:20 +0200

    tttttttt

--- resources/js/components/Vacations/AddVacationComponent.vue
+++ resources/js/components/Vacations/AddVacationComponent.vue
@@ -10,10 +10,18 @@
                 </v-card-title>
                 <v-card-text>
                     <v-form class="my-3">
-                        <v-textarea variant="outlined" :rows="1" auto-grow
-                            :rules="[(v) => !!v || 'This field is required']"></v-textarea>
-                        <v-select :label="$t('SelectStation')" :items="['stations', 'stations2']"
-                            :rules="[(v) => !!v || 'This field is required']"></v-select>
+                        <v-select label="type Request" :items="typeRequest" v-model="addRequest.type"
+                            :rules="[(v) => !!v || 'This field is required']">
+                        </v-select>
+                        <v-textarea label="description" v-model="addRequest.description" variant="outlined" :rows="1"
+                            auto-grow :rules="[(v) => !!v || 'This field is required']">
+                        </v-textarea>
+                        <v-text-field label="Date Start" type="date" v-model="addRequest.dateStart"></v-text-field>
+                        <v-text-field label="Date End" type="date" v-model="addRequest.dateEnd"></v-text-field>
+                        <v-text-field label="location" variant="outlined" v-model="addRequest.location"
+                            :rules="[(v) => !!v || 'This field is required']">
+                        </v-text-field>
+                        <h3>{{ addRequest }}</h3>
                     </v-form>
                 </v-card-text>
                 <v-card-actions>
@@ -30,6 +38,61 @@
     </v-row>
 </template>
 <script setup>
-import { ref } from "vue";
+import { usemainStore } from "../../store/mainStore";
+const store = usemainStore();
+import { ref, onMounted } from "vue";
 const dialog = ref(false);
+const typeRequest = ref([
+    "Regular",
+    "Rest allowance",
+    "Casual",
+    "Umrah leave",
+    "Recruitment",
+    "Maternity leave",
+    "Compensatory leave",
+    "Other",
+]);
+// const typeRequest = ref({
+//     "Regular": "Regular",
+//     "Rest allowance": "Rest allowance",
+//     "Casual": "Casual",
+//     "Umrah leave": "Umrah leave",
+//     "Recruitment": "Recruitment",
+//     "Maternity leave": "Maternity leave",
+//     "Compensatory leave": "Compensatory leave",
+//     "Other": "Other",
+// });
+
+// اعتيادية	Regular
+//  بدل راحة	Rest allowance
+//  عارضة	Casual
+//  اجازة عمرة	Umrah leave
+//  تجنيد	Recruitment
+//  اجازة وضع	Maternity leave
+//  اجازة تعويضية	Compensatory leave
+//  اخري	Other
+// const location = ref(null);
+const addRequest = ref({});
+onMounted(() => {
+    addRequest.value.dateStart = store.formatDate(new Date());
+    // addRequest.value.dateEnd = store.formatDate(new Date());
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            async (position) => {
+                const { latitude, longitude } = position.coords;
+                const response = await fetch(
+                    `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`
+                );
+                const data = await response.json();
+                // console.log(data);
+                addRequest.value.location = `${data.address.state}, ${data.address.town}`;
+            },
+            (error) => {
+                console.error("فشل في الحصول على الموقع:", error.message);
+            }
+        );
+    } else {
+        console.error("المتصفح الخاص بك لا يدعم Geolocation API");
+    }
+});
 </script>
