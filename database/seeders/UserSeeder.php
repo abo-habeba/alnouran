d08e562dbce17f97f0ae4255beac1a6c20feeafd
abo habeba <atsh.phone@gmail.com>
2024-02-08 18:01:27 +0200

    bvcx

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,9 +21,27 @@ class UserSeeder extends Seeder
             DB::table('users')->insert([
                 'name' => 'admin',
                 'phone' => '01234567891',
+                'roles' => 'admin',
+                'created_at' => now(),
                 'Job_title' => 'engeneer',
                 'email' => 'admin@admin.com',
                 'password' => Hash::make('12345678'),
+            ]);
+        }
+        $rest = DB::table('rest_balances')->where('user_id', 1)->first();
+        if (!$rest) {
+            DB::table('rest_balances')->insert([
+                'balance' => 0,
+                'user_id' => 1,
+                'created_at' => now(),
+            ]);
+        }
+        $regular = DB::table('regular_balances')->where('user_id', 1)->first();
+        if (!$regular) {
+            DB::table('regular_balances')->insert([
+                'balance' => 0,
+                'user_id' => 1,
+                'created_at' => now(),
             ]);
         }
     }
